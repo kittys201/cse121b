06a360f03e959958f934e59caf3cb64df7c6bfdb
Jennifer Gonzalez <kittys201@gmail.com>
2024-01-20 03:05:36 -0300

    week 2 task

---
+++ scripts/w02-task.js
@@ -0,0 +1,42 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Jennifer Gonzalez"
+let currentYear = new Date().getFullYear()
+let profilePicture = "images/kitty_sposa_300.jpg"
+let fileName = "profile picture"
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name')
+const foodElement = document.getElementById('food')
+const	yearElement = document.querySelector('#year')
+const imageElement = document.querySelector('img')
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`
+yearElement.textContent = currentYear
+imageElement.setAttribute('src', profilePicture)
+imageElement.setAttribute('alt', `Profile image of ${fullName}`)
+
+/* Step 5 - Array */
+let favFoods = ['grape ', 'banana ', 'cherry ', 'pear ']
+foodElement.innerHTML = favFoods
+let favFood = 'apple '
+favFoods.push(favFood)
+foodElement.innerHTML += `<br>${favFoods}`
+favFoods.shift()
+foodElement.innerHTML += `<br>${favFoods}`
+favFoods.pop()
+foodElement.innerHTML += `<br>${favFoods}`
+
+
+
+
+
+
+
+
+
+
